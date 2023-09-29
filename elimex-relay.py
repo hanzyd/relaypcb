2320173e587ae8db7dd68be5373df61c4c233d33
Ivan T. Ivanov <iivanov@suse.de>
2023-09-29 16:07:24 +0300

    Add command line arguments

--- elimex-relay.py
+++ elimex-relay.py
@@ -1,18 +1,18 @@
 import socket
 import json
 from time import sleep
+import argparse
 
-HOST = "192.168.5.10"  # The server's hostname or IP address
 PORT = 80  # The port used by the server
 
 CMD_EP = 'POST /web_control.lua \r\n'
 READ = 'Content-Length: 32\r\n\r\n{"id":"driveRel","action":"get"}'
 TOGGLE = 'Content-Length: 30\r\n\r\n{"id":"driveRel","button":'
 
-def send_command(command: str, verbose=False):
+def send_command(host: str, command: str, verbose=False):
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-            sock.connect((HOST, PORT))
+            sock.connect((host, PORT))
             buffer = bytes(CMD_EP + command, 'ascii')
             sock.sendall(buffer)
             data = sock.recv(8*1024)
@@ -31,21 +31,21 @@ def send_command(command: str, verbose=False):
             print("Socket error %s." %(err))
         return (False, "")
 
-def toggle_button(number):
+def toggle_button(host, number):
     command = TOGGLE + "{}".format(number) + '}'
-    send_command(command)
+    send_command(host, command)
 
-def switch_on_all_button():
-    toggle_button(9)
+def switch_on_all_button(host, ):
+    toggle_button(host, 9)
 
-def switch_off_all_buttons():
-    toggle_button(10)
+def switch_off_all_buttons(host, ):
+    toggle_button(host, 10)
 
-def read_button_state(number):
+def read_button_state(host, number):
     if number < 1 or number > 8:
         return False, False
 
-    ok, response = send_command(READ)
+    ok, response = send_command(host, READ)
     if not ok:
         return False, False
 
@@ -54,60 +54,73 @@ def read_button_state(number):
     state = True if status & 1 << (number - 1) else False
     return True, state
 
-def switch_on_button(number):
+def switch_on_button(host, number):
 
     if number < 1 or number > 8:
         return
 
-    ok, on = read_button_state(number)
+    ok, on = read_button_state(host, number)
     if not ok or on:
         return
 
-    command = TOGGLE + "{}".format(number) + '}'
-    send_command(command)
+    toggle_button(host, num)
 
-def switch_off_button(number):
+def switch_off_button(host, number):
 
     if number < 1 or number > 8:
         return
 
-    ok, on = read_button_state(number)
+    ok, on = read_button_state(host, number)
     if not ok or not on:
         return
 
-    command = TOGGLE + "{}".format(number) + '}'
-    send_command(command)
+    toggle_button(host, num)
 
-if __name__ == '__main__':
+def test_api(host):
 
-    switch_off_all_buttons()
+    switch_off_all_buttons(host)
 
     while True:
 
-        send_command(READ)
+        send_command(host, READ)
         for num in range (1, 9):
-            switch_on_button(num)
+            switch_on_button(host, num)
 
-        switch_on_button(4)
+        switch_on_button(host, 4)
 
-        send_command(READ)
+        send_command(host, READ)
         for num in range(1, 9):
-            toggle_button(num)
+            toggle_button(host, num)
 
-        switch_off_button(5)
+        switch_off_button(host, 5)
 
-        send_command(READ)
+        send_command(host, READ)
         for num in range(1, 9):
-            toggle_button(num)
+            toggle_button(host, num)
 
-        send_command(READ)
-        switch_off_all_buttons()
+        send_command(host, READ)
+        switch_off_all_buttons(host)
 
-        send_command(READ)
+        send_command(host, READ)
         for num in range (1, 9):
-            switch_off_button(num)
+            switch_off_button(host, num)
 
-pass
+if __name__ == '__main__':
+
+    args = argparse.ArgumentParser()
+    args.add_argument('--ip', dest='address', type=str, required=True,
+                      help='IP address of RelayPCB')
+    args.add_argument('--on', dest='on', action='append', type=int,
+                      choices=range(1, 9), help='Turn ON relay <number>')
+    args.add_argument('--off', dest='off', action='append', type=int,
+                      choices=range(1, 9), help='Turn OFF relay <number>')
+    args = args.parse_args()
+
+    for num in args.on:
+        switch_on_button(args.address, num)
+
+    for num in args.off:
+        switch_off_button(args.address, num)
 
 
 
